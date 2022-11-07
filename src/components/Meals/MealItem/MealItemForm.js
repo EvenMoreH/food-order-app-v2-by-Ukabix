4af722ff96c63f36676cb61dde2f8bbe3d9940f8
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-11-07 21:08:03 +0100

    add and mount MealItemForm

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,12 @@
+import styles from './MealItemFOrm.module.css';
+
+const MealItemForm = (props) => {
+	return (
+		<form className={styles.form}>
+			<input />
+			<button>Add</button>
+		</form>
+	);
+};
+
+export default MealItemForm;
