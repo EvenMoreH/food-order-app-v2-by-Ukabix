0f1d4be45e9b5b718386905cc8ecf03a59fdf904
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-11-10 22:38:03 +0100

    add useState and set default state to hidden

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+	const [cartIsDisplayed, setCartIsDisplayed] = useState(false);
+
+	const cartDisplayHandler = () => {
+		setCartIsDisplayed(true);
+	};
+
+	const cartHideHandler = () => {
+		setCartIsDisplayed(false);
+	};
+
 	return (
 		<React.Fragment>
-			<Cart />
+			{cartIsDisplayed && <Cart />}
 			<Header />
 			<main>
 				<Meals />
