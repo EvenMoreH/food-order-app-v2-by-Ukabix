d67d228887a447540f079d2b3c6e93def799f1a7
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-11-11 20:08:38 +0100

    configure add and remove btns

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,10 +9,19 @@ const Cart = (props) => {
 	const cartCtx = useContext(CartContext);
 	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 	const hasItems = cartCtx.items.length > 0;
+	const cartItemRemoveHandler = (id) => {};
+	const cartItemAddHandler = (item) => {};
 	const cartItems = (
 		<ul className={styles['cart-items']}>
 			{cartCtx.items.map((item) => (
-				<CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} />
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
 			))}
 		</ul>
 	);
