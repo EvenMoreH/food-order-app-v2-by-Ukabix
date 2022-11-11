eac7c18f96f2967c06ecc8a5358c527725aa2a2f
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-11-11 20:45:42 +0100

    implement remove item from cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -10,10 +10,12 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
 	if (action.type === 'ADD_CART_ITEM') {
 		const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+		
 		const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
 		const existingCartItem = state.items[existingCartItemIndex];
 		let updatedItems;
 
+		// hadle adding items to cart
 		if (existingCartItem) {
 			const updatedItem = {
 				...existingCartItem,
@@ -30,6 +32,27 @@ const cartReducer = (state, action) => {
 			totalAmount: updatedTotalAmount,
 		};
 	}
+
+	// handle removing cart items
+	if (action.type === 'REMOVE_CART_ITEM') {
+		const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
+		const existingItem = state.items[existingCartItemIndex];
+		const updatedTotalAmount = state.totalAmount - existingItem.price;
+		let updatedItems;
+		if (existingItem.amount === 1) {
+			updatedItems = state.items.filter((item) => item.id !== action.id);
+		} else {
+			const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		}
+
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+
 	return defaultCartState;
 };
 
