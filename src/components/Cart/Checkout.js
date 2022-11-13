ec31c0256b1a202a30913fa945f712ca58ebbd19
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-11-13 23:10:39 +0100

    implement post order to backend

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -48,6 +48,12 @@ const Checkout = (props) => {
 			return;
 		}
 		// POST data
+		props.onConfirm({
+			name: enteredName,
+			street: enteredStreet,
+			city: enteredCity,
+			postalCode: enteredPostal,
+		});
 	};
 	const nameControlStyles = `${styles.control} ${formInputsValidity.name ? '' : styles.invalid}`;
 	const streetControlStyles = `${styles.control} ${
