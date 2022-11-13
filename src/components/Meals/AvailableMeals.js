30a2b37c66e3cda21bb4edba97d6118fb51de98b
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-11-13 21:26:24 +0100

    add error handling for http req

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,14 +7,19 @@ import styles from './AvailableMeals.module.css';
 const AvailableMeals = () => {
 	const [meals, setMeals] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [httpError, setHttpError] = useState();
 
 	// reminder - you should avoid using async in useEffect
 	useEffect(() => {
 		const fetchMeals = async () => {
 			// setIsLoading(true);
 			const response = await fetch(
-				'https://react-app-v2-b7bcb-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
+				'https://react-app-v2-b7bcb-default-rtdb.europe-west1.firebasedatabase.app/meals.'
 			);
+			// check response for error
+			if (!response.ok) {
+				throw new Error('Something went wrong, please try again');
+			}
 			const responseData = await response.json();
 			const loadedMeals = [];
 			for (const key in responseData) {
@@ -28,14 +33,32 @@ const AvailableMeals = () => {
 			setMeals(loadedMeals);
 			setIsLoading(false);
 		};
-
-		fetchMeals();
+		// this sould be async and could be exported to fn outside of scope
+		// try {
+		// 	fetchMeals();
+		// } catch (error) {
+		// 	setIsLoading(false);
+		// 	setHttpError(error);
+		// }
+		// another approach - good practice to handle promises
+		fetchMeals().catch((error) => {
+			setIsLoading(false);
+			setHttpError(error);
+		});
 	}, []);
 	// handle loading state
 	if (isLoading) {
 		return (
 			<section className={styles.MealsLoading}>
 				<p>Loading, please wait</p>
+			</section>
+		);
+	}
+	// handle error state
+	if (httpError) {
+		return (
+			<section className={styles.MealsError}>
+				<p>{httpError}</p>
 			</section>
 		);
 	}
